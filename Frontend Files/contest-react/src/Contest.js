b69a36bced789efb8cdfa34f7d22a3340a45f5b0
kdm423 <kdm423@nau.edu>
2023-07-29 20:38:47 -0700

    Start of the frontend of Contests
    front end react setup

---
+++ Frontend Files/contest-react/src/Contest.js
@@ -0,0 +1,35 @@
+//IMPORTS
+import React,{Component} from 'react';
+
+// CSS Fonts Import
+<style>
+import url('https://fonts.googleapis.com/css2?
+family=Lilita+One&
+family=Teko:wght@600&
+display=swap');
+</style>
+
+///////////////////////////////////////////////////////////////////////////////
+
+////////////////////////
+/* Main Contest Class */
+////////////////////////
+
+class Contest extends Component {
+    // Preview contests???
+    // Showcase contests that are closing soon (no more entries)?
+    // Showcase contests that have voting closing soon (no more votes on entries)??
+
+    // render
+    render() {
+        <div>
+            <h1> Fitz Contests</h1>
+            <h2> Vote on your fave styles or enter your own looks!</h2>
+            <div> Vote! </div>
+            <div> Compete!</div>
+        </div>
+    }
+
+}
+
+export default Contest;
